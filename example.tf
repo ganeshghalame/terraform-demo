d8457a68cb4504cfdf7405c0b8870222c8f2ef06
Ganesh Ghalame <ganesh.ghalame@motifworks.com>
2020-02-20 16:55:54 +0530

    provisioner local-exec  for getting IP address

--- example.tf
+++ example.tf
@@ -8,4 +8,8 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   # vpc_security_group_ids = ["sg-0077..."]
   # subnet_id = "subnet-923a..."
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
